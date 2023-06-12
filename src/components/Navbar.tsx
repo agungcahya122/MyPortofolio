b501c3a5d62ebb66bec94529449e709c15f9aced
Agung Cahya <agungcahya122@gmail.com>
2023-06-12 13:03:45 +0700

    style: fix deploy email message

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
       </div>
       <div className="navbar-center flex">
         <ul className="flex justify-center gap-9 font-medium text-zinc-900 text-[18px] hover:cursor-pointer">
-          <li className={position === "aboutme" ? "text-blue-100" : "hover:text-hijau"}><Link onClick={() => setPosition("aboutme")} to="aboutme" spy={true} smooth={true} offset={-80} duration={800} >About me</Link></li>
+          <li className={position === "aboutme" ? "text-hijau" : "hover:text-hijau"}><Link onClick={() => setPosition("aboutme")} to="aboutme" spy={true} smooth={true} offset={-80} duration={800} >About me</Link></li>
           <li className={position === "projects" ? "text-hijau" : "hover:text-hijau"}><Link onClick={() => setPosition("projects")} to="projects" spy={true} smooth={true} offset={-80} duration={800} >Projects</Link></li>
           <li className={position === "contact" ? "text-hijau" : "hover:text-hijau"}><Link onClick={() => setPosition("contact")} to="contact" spy={true} smooth={true} offset={-80} duration={800}>Contact</Link></li>
         </ul>
